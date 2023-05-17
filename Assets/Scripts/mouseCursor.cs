7610ff625ba64980c71ebbf3e98326a884f41b0e
Aurelien Tavernier <aurelientav@gmail.com>
2023-05-17 10:11:47 +0200

    can destroy ui at a certein point

---
+++ Assets/Scripts/mouseCursor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseCursor : MonoBehaviour
+{
+    public DragAndDrop dragAndDrop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Cursor.lockState = CursorLockMode.Locked ;   
+        Cursor.visible = false;
+       // StartCoroutine(waitStop());
+    }
+
+    // Update is called once per frame
+    public IEnumerator waitStop()
+    {
+        yield return new WaitForSeconds(1f);
+        dragAndDrop.CanMoveCursor = false;
+    }
+}
