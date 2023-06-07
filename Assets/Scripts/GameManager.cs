270f571e496c7c7a3e0ad822d426f08c38071f4e
Aurelien Tavernier <aurelientav@gmail.com>
2023-06-07 10:59:32 +0200

    animation hand tableau 2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,6 +13,8 @@ public class GameManager : MonoBehaviour
     public List<GameObject> SimonUI = new List<GameObject>();
     public List<GameObject> breakableUI = new List<GameObject>();
     public GameObject StockCD;
+    public GameObject Narrator;
+    
     public Physics2DRaycaster Raycaster2D;
     string tableau;
     private void Awake()
@@ -52,5 +54,31 @@ public class GameManager : MonoBehaviour
         }
         StockCD.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
         StockCD.tag = "Object";
+    }
+
+    public void TouchCD(int numberOfTouch)
+    {
+        switch (numberOfTouch)
+        {
+            case 0:
+                Debug.Log("touche une fois");
+                Narrator.GetComponent<Animator>().SetInteger();
+               // timing = 0.5f;
+                break;
+            case 1:
+                Debug.Log("touche une seconde fois");
+               // timing = 0.4f;
+                break;
+            case 2:
+                Debug.Log("Detruit l'UI");
+               // timing = 0.3f;
+                //nous permet de rendre la souris invisible et non utilisable
+                Cursor.lockState = CursorLockMode.Locked;
+
+                //On va utiliser un faux curseur pour empecher le joueur de l'utiliser
+              /*  cursor.SetActive(true);
+                cursor.transform.position = new Vector3(GetComponent<Raycast>().HitToStopMouse.point.x, GetComponent<Raycast>().HitToStopMouse.point.y, 0f);*/
+                break;
+        }
     }
 }
