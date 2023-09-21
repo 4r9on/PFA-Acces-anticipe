f6db9d30d5b9c33713e2e65968a432c00ce35b7f
Aurelien Tavernier <aurelientav@gmail.com>
2023-09-21 16:33:51 +0200

    books Changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -68,6 +68,11 @@ public class GameManager : MonoBehaviour
     public GameObject night;
     public List<GameObject> DayNight = new List<GameObject>();
     public List<GameObject> DigicodeView = new List<GameObject>();
+    public List<GameObject> AllBooks = new List<GameObject>();
+    public List<Sprite> BooksSpriteXL = new List<Sprite>();
+    public List<Sprite> BooksSpriteBig = new List<Sprite>();
+    public List<Sprite> BooksSpriteMedium = new List<Sprite>();
+    public List<Sprite> BooksSpriteSmall = new List<Sprite>();
     public GameObject dayLight;
     public GameObject Digicode;
     public GameObject nightLight;
@@ -674,6 +679,38 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void ChangeSpriteBook(GameObject book)
+    {
+        bool sizeIsFind = false;
+        sizeIsFind = FindTheGoodSize(book, BooksSpriteXL);
+        if (!sizeIsFind)
+        {
+            sizeIsFind = FindTheGoodSize(book, BooksSpriteBig);
+        }
+        if (!sizeIsFind)
+        {
+            sizeIsFind = FindTheGoodSize(book, BooksSpriteMedium);
+        }
+        if (!sizeIsFind)
+        {
+            sizeIsFind = FindTheGoodSize(book, BooksSpriteSmall);
+        }
+    }
 
+    public bool FindTheGoodSize(GameObject book, List<Sprite> BooksSpriteSize)
+    {
+        bool sizeIsFind = false;
+        foreach (Sprite bookSprite in BooksSpriteSize)
+        {
+            if (book.GetComponent<SpriteRenderer>().sprite == bookSprite)
+            {
+                sizeIsFind = true;
+                List<Sprite> booksSprite = new List<Sprite>();
+                booksSprite.Remove(bookSprite);
+                book.GetComponent<SpriteRenderer>().sprite = BooksSpriteSize[Random.Range(0, BooksSpriteSize.Count)];
+            }
+        }
+        return sizeIsFind;
+    }
 }
 
